f4296931c48f6cf47b8c624027d8dbd74604d307
Brian.Chu <chuchengpeng0119@163.com>
2018-12-11 11:19:42 +0800

    Initial Commit

---
+++ CCPCUrl/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CCPCUrl
+//
+//  Created by 储诚鹏 on 2018/12/11.
+//  Copyright © 2018 储诚鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CCPCUrl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCPCUrl
+//
+//  Created by 储诚鹏 on 2018/12/11.
+//  Copyright © 2018 储诚鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
